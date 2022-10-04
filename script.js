af0fca0d53085a79bf49c2e9b96875191c933ad3
Your Name <you@example.com>
2022-10-04 20:14:03 -0300

    alterações no tamanho, primeira versão das opções de áreas de atuação e primeira versão do js

---
+++ script.js
@@ -0,0 +1,44 @@
+function openPopUp1() { //mostrar menu popup "HOME CARE" no display
+        document.querySelector("#ctf1").style.display = "block";   
+}
+
+function openPopUp2() { //mostrar menu popup "FISOTERAPIA" no display
+    document.querySelector("#ctf2").style.display = "block";   
+}
+
+function openPopUp3() { //mostrar menu popup "NUTRIÇÃO" no display
+    document.querySelector("#ctf3").style.display = "block";   
+}
+
+function openPopUp4() { //mostrar menu popup "FISOTERAPIA" no display
+    document.querySelector("#ctf4").style.display = "block";   
+}
+
+function openPopUp5() { //mostrar menu popup "FISOTERAPIA" no display
+    document.querySelector("#ctf5").style.display = "block";   
+}
+
+function openPopUp6() { //mostrar menu popup "FISOTERAPIA" no display
+    document.querySelector("#ctf6").style.display = "block";   
+}
+
+function openPopUp7() { //mostrar menu popup "FISOTERAPIA" no display
+    document.querySelector("#ctf7").style.display = "block";   
+}
+
+function openPopUp8() { //mostrar menu popup "FISOTERAPIA" no display
+    document.querySelector("#ctf8").style.display = "block";   
+}
+
+function closePopUp() {//ocultar menu popup no display
+   
+    document.querySelector("#ctf1").style.display = "none";
+    document.querySelector("#ctf2").style.display = "none";
+    document.querySelector("#ctf3").style.display = "none";
+    document.querySelector("#ctf4").style.display = "none";
+    document.querySelector("#ctf5").style.display = "none";
+    document.querySelector("#ctf6").style.display = "none";
+    document.querySelector("#ctf7").style.display = "none";
+    document.querySelector("#ctf8").style.display = "none";
+           
+}
