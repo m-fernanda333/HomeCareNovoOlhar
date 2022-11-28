e4e88c6ceb58e3b8fb2b481b8a2f133fc4b9f78d
Your Name <you@example.com>
2022-11-28 01:08:35 -0300

    ideia de sticky

--- script.js
+++ script.js
@@ -22,54 +22,63 @@ function openPopUp1() { //mostrar menu popup "HOME CARE" no display
         document.querySelector("#ctf1").style.display = "block"; 
         document.querySelector(".wrap").style.overflowY = "hidden";
         document.querySelector("body").style.overflowY = "hidden";
+        document.querySelector(".navBar").style.display = "none";
 }
 
 function openPopUp2() { //mostrar menu popup "FISOTERAPIA" no display
     document.querySelector("#ctf2").style.display = "block";   
     document.querySelector(".wrap").style.overflowY = "hidden";
     document.querySelector("body").style.overflowY = "hidden";
+    document.querySelector(".navBar").style.display = "none";
 }
 
 function openPopUp3() { //mostrar menu popup "NUTRIÇÃO" no display
     document.querySelector("#ctf3").style.display = "block"; 
     document.querySelector(".wrap").style.overflowY = "hidden";
-    document.querySelector("body").style.overflowY = "hidden";  
+    document.querySelector("body").style.overflowY = "hidden";
+    document.querySelector(".navBar").style.display = "none";  
 }
 
 function openPopUp4() { //mostrar menu popup "PSICOLOGIA" no display
     document.querySelector("#ctf4").style.display = "block";
     document.querySelector(".wrap").style.overflowY = "hidden";
     document.querySelector("body").style.overflowY = "hidden";   
+    document.querySelector(".navBar").style.display = "none";
 }
 
 function openPopUp5() { //mostrar menu popup "ENFERMAGEM" no display
     document.querySelector("#ctf5").style.display = "block";  
     document.querySelector(".wrap").style.overflowY = "hidden";
     document.querySelector("body").style.overflowY = "hidden"; 
+    document.querySelector(".navBar").style.display = "none";
 }
 
 function openPopUp6() { //mostrar menu popup "FONOAUDIOLOGIA" no display
     document.querySelector("#ctf6").style.display = "block";  
     document.querySelector(".wrap").style.overflowY = "hidden";
     document.querySelector("body").style.overflowY = "hidden"; 
+    document.querySelector(".navBar").style.display = "none";
 }
 
 function openPopUp7() { //mostrar menu popup "TRANSPORTE AMBULATORIAL" no display
     document.querySelector("#ctf7").style.display = "block";   
     document.querySelector(".wrap").style.overflowY = "hidden";
     document.querySelector("body").style.overflowY = "hidden";
+    document.querySelector(".navBar").style.display = "none";
 }
 
 function openPopUp8() { //mostrar menu popup "ATENDIMENTO MÉDICO" no display
     document.querySelector("#ctf8").style.display = "block";   
     document.querySelector(".wrap").style.overflowY = "hidden";
     document.querySelector("body").style.overflowY = "hidden";
+    document.querySelector(".navBar").style.display = "none";
 }
 
 function openPopUp9() { //mostrar menu popup "DESENVOLVIMENTO PROFISSIONAL" no display
     document.querySelector("#ctf9").style.display = "block";   
     document.querySelector(".wrap").style.overflowY = "hidden";
     document.querySelector("body").style.overflowY = "hidden";
+    document.querySelector(".navBar").style.display = "none";
 }
 
 function closePopUp() {//ocultar menu popup no display
@@ -85,6 +94,7 @@ function closePopUp() {//ocultar menu popup no display
     document.querySelector("#ctf9").style.display = "none";
     document.querySelector(".wrap").style.overflowY = "initial";
     document.querySelector("body").style.overflowY = "initial";
+    document.querySelector(".navBar").style.display = "flex";
            
 }
 
@@ -161,7 +171,19 @@ function slider3 () {
 
 }
 
+
 Window.sr = ScrollReveal({ reset: true });
 ScrollReveal().reveal('.delayHardReveal', { delay: 300, origin: "top",distance: "50px", duration: 1000 });
 ScrollReveal().reveal('.delayMediumReveal', { delay: 200, origin: "top",distance: "50px", duration: 500 });
 ScrollReveal().reveal('.delayFastReveal', { delay: 100, origin: "top",distance: "50px", duration: 200 });
+
+
+
+
+
+
+window.addEventListener('scroll', function(){
+    var navBar = document.querySelector('.navBar');
+    navBar.classList.remove('delayMediumReveal');
+    navBar.classList.toggle('sticky', window.scrollY > 0);
+  })
